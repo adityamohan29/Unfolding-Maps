e8f65148cb6b0efaae8a3a1007e701bc246dea32
spacewafer <32217637+spacewafer@users.noreply.github.com>
2019-07-25 22:31:57 +0530

    Add files via upload

---
+++ CommonMarker.java
@@ -0,0 +1,63 @@
+package module6;
+
+
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import processing.core.PGraphics;
+
+/** Implements a common marker for cities and earthquakes on an earthquake map
+ * 
+ * @author UC San Diego Intermediate Software Development MOOC team
+ *
+ */
+public abstract class CommonMarker extends SimplePointMarker {
+
+	// Records whether this marker has been clicked (most recently)
+	protected boolean clicked = false;
+	
+	public CommonMarker(Location location) {
+		super(location);
+	}
+	
+	public CommonMarker(Location location, java.util.HashMap<java.lang.String,java.lang.Object> properties) {
+		super(location, properties);
+	}
+	
+	// Getter method for clicked field
+	public boolean getClicked() {
+		return clicked;
+	}
+	
+	// Setter method for clicked field
+	public void setClicked(boolean state) {
+		clicked = state;
+	}
+	//
+	public static float tc1;
+	public static void gettc(double tc)
+	{
+		tc1=(float)tc;
+	}
+	// Common piece of drawing method for markers; 
+	// YOU WILL IMPLEMENT. 
+	// Note that you should implement this by making calls 
+	// drawMarker and showTitle, which are abstract methods 
+	// implemented in subclasses
+	public void draw(PGraphics pg, float x, float y) {
+		// For starter code just drawMaker(...)
+		if (!hidden) {
+			drawMarker(pg, x, y);
+			if (selected) {
+				showTitle(pg, x, y);
+				//float f=Float.parseFloat(getId()); 
+				pg.ellipse(x,y,tc1/10,tc1/10);
+				//System.out.println(em.tc);
+			}
+			
+		}
+	}
+	
+	public abstract void drawMarker(PGraphics pg, float x, float y);
+	public abstract void showTitle(PGraphics pg, float x, float y);
+}
